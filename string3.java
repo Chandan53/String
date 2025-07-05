7ebb9e46bc43fe63e7f530c294bc1b20edbad89f
chandan <chandankumar936672@gmail.com>
2025-07-05 22:04:40 +0530

    using Length() function in string

---
+++ string3.java
@@ -0,0 +1,10 @@
+package String;
+// length()--> length function in java
+public class string3 {
+    public static void main(String args[]){
+        String name ="hell every one well come to chandan repo";
+        //  we can find the number of character present in our string using length
+        System.out.println(name.length());
+    }
+    
+}
