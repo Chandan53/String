96590ad3e034700594a7e4a3ce33b5c23d089b7b
chandan <chandankumar936672@gmail.com>
2025-07-05 22:09:45 +0530

    concatenating two string

---
+++ string4.java
@@ -0,0 +1,12 @@
+package String;
+
+public class string4 {
+    // concatenation --> means adding two string is called concatenation
+    public static void main(String args[]){
+        String first_name ="chandan";
+        String last_name="kumar";
+        String full_name = first_name  + "  " + last_name;
+        System.out.println(full_name); 
+    }
+    
+}
