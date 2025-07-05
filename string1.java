dd6415cf5ae031ae023b012dae5c424575ca4861
chandan <chandankumar936672@gmail.com>
2025-07-05 21:41:58 +0530

    method to create a String

---
+++ string1.java
@@ -0,0 +1,18 @@
+package String;
+
+public class string1 {
+    public static void main(String args[]){
+        // two methot to create a String4
+        String srt =("abcd");
+        String srt1=new String ("xys");
+        // character array 
+        char arr[] = {'a','b','c','d','e'};
+
+        System.out.println(srt);
+        System.out.println(srt1);
+        for(int i=0;i<arr.length;i++){
+            System.out.println(arr[i]);
+        }
+    }
+    
+}
