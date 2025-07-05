208e669c73b09fea9b3b415742dd84e3781d13c7
chandan <chandankumar936672@gmail.com>
2025-07-05 22:19:01 +0530

    accessing the character from the string

---
+++ string5.java
@@ -0,0 +1,15 @@
+package String;
+// accesiing the character from a string
+public class string5 {
+    public static void print(String full_name){
+        for(int i=0;i<=full_name.length()-1;i++){
+            System.out.print(full_name.charAt(i) +" ");
+        }
+        System.out.println();
+    }
+    public static void main(String args[]){
+        String full_name = "chandan kumar";
+        print(full_name);
+    }
+    
+}
