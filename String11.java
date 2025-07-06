56c9df3bb31bf755f3d1aaf9cf4859ad2d5fcfcf
chandan <chandankumar936672@gmail.com>
2025-07-06 18:22:32 +0530

    Lexicographic order finding the largest string

---
+++ String11.java
@@ -0,0 +1,15 @@
+package String;
+// Q for a given set of string print the largest string
+public class String11 {
+    public static void main(String args[]){
+        String fruits[]={"apple","mango","banana"};
+        String largest=fruits[0];
+        for(int i=1;i<fruits.length;i++){
+            if(largest.compareTo(fruits[i])<0){
+                largest=fruits[i];
+            }
+        }
+        System.out.println(largest);
+    }
+    
+}
