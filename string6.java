b7ced08bd53c3948c75606882fe7eb32755d5ada
chandan <chandankumar936672@gmail.com>
2025-07-06 11:35:47 +0530

    string is a palindrome or not

---
+++ string6.java
@@ -0,0 +1,25 @@
+package String;
+
+public class string6 {
+    // palindrome --> it is a word that when we read from the start is same as when we read from the end
+    // eg ;- r a c e c a r , n o o n, m a d a m
+    public static boolean palindrome(String str){
+        int n=str.length();
+        for(int i=0; i<str.length()/2;i++){
+            
+            if(str.charAt(i) != str.charAt(n-1-i)){
+                return false;
+
+            }
+            
+
+        }
+        return true;
+        
+    }
+    public static void main(String args[]){
+        String str="racpar";
+        System.out.println(palindrome(str));
+    }
+    
+}
